0351e1d637478d0cb64876eea597db34da1cd377
manishkumar8312 <codeisfunforme@gmail.com>
2025-06-27 19:06:42 +0530

    first commit

---
+++ client/src/components/StarRating.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const StarRating = ({ rating = 5 }) => {
+  return (
+    <div className="flex items-center">
+      {Array.from({ length: 5 }, (_, i) => (
+        <img key={i} src={i < rating ? assets.starIconFilled : assets.starIconOutlined} alt="star" className="w-4.5 h-4.5" />
+      ))}
+    </div>
+  )
+}
+
+export default StarRating
